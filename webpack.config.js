1302ca89180cfc306535203ef5caefa2a708896f
zlargon <zlargon@icloud.com>
2016-07-21 03:20:26 +0800

    fix warning in production mode
    http://dev.topheman.com/make-your-react-production-minified-version-with-webpack/
    
    Warning: It looks like you're using a minified copy of the development build of React. When deploying React apps to production, make sure to use the production build which skips development warnings and is faster. See https://fb.me/react-minification for more details.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: 'bundle.js'
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   module: {
     loaders: [
       {
